f63112c77be4477a41a3c53be6e22a20bacda44f
Anthony Adam <thonyadam@gmail.com>
2020-04-07 20:49:17 -0300

    formularios de cadastro v1.0

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdmPageComponent } from './adm-page/adm-page.component';
+import { FormAlunoComponent } from './form-aluno/form-aluno.component';
+import { FormMedicoComponent } from './form-medico/form-medico.component';
+import { FormProfessorComponent } from './form-professor/form-professor.component';
 
 
 const routes: Routes = [
@@ -12,6 +15,9 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'adm', component: AdmPageComponent },
+  { path: 'aluno', component: FormAlunoComponent },
+  { path: 'medico', component: FormMedicoComponent },
+  { path: 'professor', component: FormProfessorComponent },
 ];
 
 @NgModule({
