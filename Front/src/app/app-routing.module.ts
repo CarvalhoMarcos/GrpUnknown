a798ff389c8102ddb7e9e3be065e7e51e3a01eed
Anthony Adam <thonyadam@gmail.com>
2020-05-03 22:18:59 -0300

    ajuste crud aluno
    ajustada tela e request de cadastro

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdmPageComponent } from './adm-page/adm-page.component';
 import { CadastroAlunoComponent } from './aluno/cadastro-aluno/cadastro-aluno.component';
+import { CadastroMedComponent } from './med/cadastro-med/cadastro-med.component';
+import { CadastroProfComponent } from './prof/cadastro-prof/cadastro-prof.component';
 
 
 
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'adm', component: AdmPageComponent },
   { path: 'adm/cad-aluno', component: CadastroAlunoComponent },
+  { path: 'adm/cad-med', component: CadastroMedComponent },
+  { path: 'adm/cad-prof', component: CadastroProfComponent },
 ];
 
 @NgModule({
