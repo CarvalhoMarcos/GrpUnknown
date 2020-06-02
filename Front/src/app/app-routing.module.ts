4473312d80cb6f5e127137cd20ff45b72226f39e
Lucas <lucasnbarros@gmail.com>
2020-06-02 07:55:40 -0300

    nova branch, teste de componente

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,64 +1,70 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { AdmPageComponent } from "./adm-page/adm-page.component";
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { AdmPageComponent } from './adm-page/adm-page.component';
+import { MedComponent } from "./med/med.component";
+import { CadastroMedComponent } from "./med/cadastro-med/cadastro-med.component";
+import { DeletarMedComponent } from "./med/deletar-med/deletar-med.component";
 
-import { MedComponent } from './med/med.component';
-import { CadastroMedComponent } from './med/cadastro-med/cadastro-med.component';
-import { DeletarMedComponent } from './med/deletar-med/deletar-med.component';
+import { AlunoComponent } from "./aluno/aluno.component";
+import { CadastroAlunoComponent } from "./aluno/cadastro-aluno/cadastro-aluno.component";
+import { DeletarAlunoComponent } from "./aluno/deletar-aluno/deletar-aluno.component";
 
-import { AlunoComponent } from './aluno/aluno.component';
-import { CadastroAlunoComponent } from './aluno/cadastro-aluno/cadastro-aluno.component';
-import { DeletarAlunoComponent } from './aluno/deletar-aluno/deletar-aluno.component';
+import { ProfComponent } from "./prof/prof.component";
+import { CadastroProfComponent } from "./prof/cadastro-prof/cadastro-prof.component";
+import { DeletarProfComponent } from "./prof/deletar-prof/deletar-prof.component";
 
-import { ProfComponent } from './prof/prof.component';
-import { CadastroProfComponent } from './prof/cadastro-prof/cadastro-prof.component';
-import { DeletarProfComponent } from './prof/deletar-prof/deletar-prof.component';
+import { PacienteComponent } from "./paciente/paciente.component";
+import { CadastroPaciComponent } from "./paciente/cadastro-paci/cadastro-paci.component";
+import { DeletarPaciComponent } from "./paciente/deletar-paci/deletar-paci.component";
 
-import { PacienteComponent } from './paciente/paciente.component';
-import { CadastroPaciComponent } from './paciente/cadastro-paci/cadastro-paci.component';
-import { DeletarPaciComponent } from './paciente/deletar-paci/deletar-paci.component';
-
-
-
-
+import { LaudoMedicoComponent } from "./laudo-medico/laudo-medico.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'adm', component: AdmPageComponent },
-  { path: 'adm/med', component: MedComponent,
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "adm", component: AdmPageComponent },
+  {
+    path: "adm/med",
+    component: MedComponent,
     children: [
-      { path: 'cad-med', component: CadastroMedComponent },
-      { path: 'del-med', component: DeletarMedComponent },
-    ]
+      { path: "cad-med", component: CadastroMedComponent },
+      { path: "del-med", component: DeletarMedComponent },
+    ],
   },
-  { path: 'adm/prof', component: ProfComponent,
+  {
+    path: "adm/prof",
+    component: ProfComponent,
     children: [
-      { path: 'cad-prof', component: CadastroProfComponent },
-      { path: 'del-prof', component: DeletarProfComponent },
-    ] 
+      { path: "cad-prof", component: CadastroProfComponent },
+      { path: "del-prof", component: DeletarProfComponent },
+    ],
   },
-  { path: 'adm/paci', component: PacienteComponent,
+  {
+    path: "adm/paci",
+    component: PacienteComponent,
     children: [
-      { path: 'cad-paci', component: CadastroPaciComponent },
-      { path: 'del-paci', component: DeletarPaciComponent },
-    ] 
+      { path: "cad-paci", component: CadastroPaciComponent },
+      { path: "del-paci", component: DeletarPaciComponent },
+    ],
   },
-  { path: 'adm/aluno', component: AlunoComponent,
+  {
+    path: "adm/aluno",
+    component: AlunoComponent,
     children: [
-      { path: 'cad-aluno', component: CadastroAlunoComponent },
-      { path: 'del-aluno', component: DeletarAlunoComponent },
-    ]
+      { path: "cad-aluno", component: CadastroAlunoComponent },
+      { path: "del-aluno", component: DeletarAlunoComponent },
+    ],
   },
+  { path: "laudo-medico", component: LaudoMedicoComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: false })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
