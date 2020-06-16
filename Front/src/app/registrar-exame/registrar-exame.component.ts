164914fab22a2f43cb21a5aa1179418ff7aa5f70
Lucas <lucasnbarros@gmail.com>
2020-06-16 12:29:25 -0300

    registro-exame: getPedidoPacienteId

--- Front/src/app/registrar-exame/registrar-exame.component.ts
+++ Front/src/app/registrar-exame/registrar-exame.component.ts
@@ -6,6 +6,8 @@ import {
   ResponseCreateRegistroExame,
 } from "./registrar-exame.model";
 import { RegistrarExameService } from "./registrar-exame.service";
+import { PedidoExameService } from "../pedido-exame/pedido-exame.service";
+import { PedidoExame } from "../pedido-exame/pedido-exame.model";
 
 @Component({
   selector: "app-registrar-exame",
@@ -13,7 +15,27 @@ import { RegistrarExameService } from "./registrar-exame.service";
   styleUrls: ["./registrar-exame.component.css"],
 })
 export class RegistrarExameComponent implements OnInit {
+  //requests/responses
+  requestCreate: RequestCreateRegistroExame = {
+    data_hora_exame: new Date().toISOString(),
+    pedido_id: "",
+  };
+
+  responseCreate: ResponseCreateRegistroExame;
+
+  responseRegistro: RegistroExame; //object ou any
+
+  responseRegistros: ResponseRegistroExame;
+
+  pedido_id: "";
+
+  // PedidoPacienteId
+  paciente_id: "";
+  responsePedido: PedidoExame;
+
   // lista de seleção
+  registrosList: Object[];
+
   // registros: Object[]; lista do tipo aberto fazer o if pra achar esses
   typesOfShoes: string[] = [
     "Boots",
@@ -22,24 +44,6 @@ export class RegistrarExameComponent implements OnInit {
     "Moccasins",
     "Sneakers",
   ];
-
-  //requests/responses
-  responseRegistro: RegistroExame;
-  // responseRegistro: Object; //object ou any
-
-  responseRegistros: ResponseRegistroExame;
-
-  requestCreate: RequestCreateRegistroExame = {
-    data_hora_exame: new Date().toISOString(),
-    pedido_id: "",
-    // status: "E", // mudar pra E??
-  };
-
-  responseCreate: ResponseCreateRegistroExame;
-
-  pedido_id: "";
-
-  registros: Object[];
 
   // upload e render do PDF
   pdfSrc = "";
@@ -58,22 +62,26 @@ export class RegistrarExameComponent implements OnInit {
     }
   }
 
-  constructor(private registrarExameService: RegistrarExameService) {}
+  constructor(
+    private registrarExameService: RegistrarExameService,
+    private pedidoExameService: PedidoExameService
+  ) {}
 
   ngOnInit(): void {
     this.getTodosRegistros();
   }
 
-  getRegistroPedidoId() {
-    this.registrarExameService
-      .getRegistroExame(this.pedido_id)
+  getPedidoPacienteId() {
+    this.pedidoExameService
+      .getPedidoExame(this.responsePedido.paciente_id)
       .subscribe((res) => {
-        this.responseRegistro = res;
+        this.responsePedido = res;
       });
   }
+
   getTodosRegistros() {
     this.registrarExameService.getAllRegistrosExame().subscribe((res) => {
-      this.registros = res;
+      this.registrosList = res;
     });
   }
 
@@ -84,5 +92,6 @@ export class RegistrarExameComponent implements OnInit {
         this.responseCreate = res;
       });
     // console.log(this.request);
+    // status: "E", // mudar pra E??
   }
 }
--- Front/src/app/registrar-exame/registrar-exame.service.ts
+++ Front/src/app/registrar-exame/registrar-exame.service.ts
@@ -7,11 +7,15 @@ import {
   ResponseCreateRegistroExame,
 } from "./registrar-exame.model";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { PedidoExameService } from "../pedido-exame/pedido-exame.service";
+import { PedidoExame } from "../pedido-exame/pedido-exame.model";
+
 @Injectable({
   providedIn: "root",
 })
 export class RegistrarExameService {
   private url = "http://localhost:4003/registrosExame";
+  // private urlpedidos = "http://localhost:4003/pedidosExame";
 
   // constructor() {}
   constructor(private http: HttpClient) {}
@@ -19,6 +23,7 @@ export class RegistrarExameService {
   // pegar pedidos de exame em aberto
   getRegistroExame(pedido_id: string): Observable<RegistroExame> {
     //get pedido exame pra pegar o cpf
+    // this.http.get<PedidoExame>(this.urlpedidos + "/" + paciente_id);
     return this.http.get<RegistroExame>(this.url + "/" + pedido_id);
   }
 
